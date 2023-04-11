b5ab78ab54fd23716f94e737c5e6ea50aea3305a
Vsssska <babkin60@yandex.ru>
2023-04-11 11:53:09 +0300

    add lab7

---
+++ Lab7/lab7/lab7/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBoxSecOp = new System.Windows.Forms.ListBox();
+            this.listBoxRes = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDell = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonClear2 = new System.Windows.Forms.Button();
+            this.buttonPerf = new System.Windows.Forms.Button();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // listBoxSecOp
+            // 
+            this.listBoxSecOp.FormattingEnabled = true;
+            this.listBoxSecOp.Location = new System.Drawing.Point(12, 108);
+            this.listBoxSecOp.Name = "listBoxSecOp";
+            this.listBoxSecOp.Size = new System.Drawing.Size(120, 95);
+            this.listBoxSecOp.TabIndex = 2;
+            // 
+            // listBoxRes
+            // 
+            this.listBoxRes.FormattingEnabled = true;
+            this.listBoxRes.Location = new System.Drawing.Point(13, 219);
+            this.listBoxRes.Name = "listBoxRes";
+            this.listBoxRes.Size = new System.Drawing.Size(120, 108);
+            this.listBoxRes.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(254, 40);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(113, 49);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonDell
+            // 
+            this.buttonDell.Location = new System.Drawing.Point(254, 95);
+            this.buttonDell.Name = "buttonDell";
+            this.buttonDell.Size = new System.Drawing.Size(113, 48);
+            this.buttonDell.TabIndex = 5;
+            this.buttonDell.Text = "Удалить";
+            this.buttonDell.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(254, 149);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(113, 50);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Первый операнд";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Второй операнд";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Результат";
+            // 
+            // buttonClear2
+            // 
+            this.buttonClear2.Location = new System.Drawing.Point(143, 235);
+            this.buttonClear2.Name = "buttonClear2";
+            this.buttonClear2.Size = new System.Drawing.Size(113, 49);
+            this.buttonClear2.TabIndex = 10;
+            this.buttonClear2.Text = "Очистить";
+            this.buttonClear2.UseVisualStyleBackColor = true;
+            this.buttonClear2.Click += new System.EventHandler(this.buttonClear2_Click);
+            // 
+            // buttonPerf
+            // 
+            this.buttonPerf.Location = new System.Drawing.Point(255, 235);
+            this.buttonPerf.Name = "buttonPerf";
+            this.buttonPerf.Size = new System.Drawing.Size(113, 49);
+            this.buttonPerf.TabIndex = 11;
+            this.buttonPerf.Text = "Выполнить";
+            this.buttonPerf.UseVisualStyleBackColor = true;
+            this.buttonPerf.Click += new System.EventHandler(this.buttonPerf_Click);
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(143, 281);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(113, 49);
+            this.buttonCalc.TabIndex = 12;
+            this.buttonCalc.Text = "Считать";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(255, 281);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(113, 49);
+            this.buttonCancel.TabIndex = 13;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 368);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.buttonPerf);
+            this.Controls.Add(this.buttonClear2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDell);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxRes);
+            this.Controls.Add(this.listBoxSecOp);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBoxSecOp;
+        private System.Windows.Forms.ListBox listBoxRes;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDell;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonClear2;
+        private System.Windows.Forms.Button buttonPerf;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
+
---
+++ Lab7/lab7/lab7/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = (char)e.KeyChar;
+            if(((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44))
+                e.Handled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                listBoxSecOp.Items.Add((i*10).ToString());
+            }
+            comboBox1.Items.Add("<"); //0 index
+            comboBox1.Items.Add(">"); //1 index
+            comboBox1.Items.Add("="); //2 index
+            comboBox1.Items.Add("<="); //3 index
+            comboBox1.Items.Add(">="); //4 index
+            comboBox1.Items.Add("<>"); //5 index
+        }
+
+        private void buttonPerf_Click(object sender, EventArgs e)
+        {
+            double f_oper=0;
+            double s_oper=0;
+            try
+            {
+                f_oper = Convert.ToDouble(textBox1.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                textBox1.Clear();
+            }
+
+            if(listBoxSecOp.SelectedItems.Count > 0)
+                s_oper = Convert.ToDouble(listBoxSecOp.SelectedItem);
+
+            try
+            {
+                if(comboBox1.SelectedIndex == 0)
+                    listBoxRes.Items.Add($"{f_oper} < {s_oper} = {f_oper < s_oper}");
+                if (comboBox1.SelectedIndex == 1)
+                    listBoxRes.Items.Add($"{f_oper} > {s_oper} = {f_oper > s_oper}");
+                if (comboBox1.SelectedIndex == 2)
+                    listBoxRes.Items.Add($"{f_oper} = {s_oper} = {f_oper == s_oper}");
+                if (comboBox1.SelectedIndex == 3)
+                    listBoxRes.Items.Add($"{f_oper} <= {s_oper} = {f_oper <= s_oper}");
+                if (comboBox1.SelectedIndex == 4)
+                    listBoxRes.Items.Add($"{f_oper} >= {s_oper} = {f_oper >= s_oper}");
+                if (comboBox1.SelectedIndex == 5)
+                    listBoxRes.Items.Add($"{f_oper} <> {s_oper} = {f_oper != s_oper}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonClear2_Click(object sender, EventArgs e)
+        {
+            listBoxRes.Items.Clear();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            string[] _temps = listBoxRes.SelectedItem.ToString().Split(' ');
+            textBox1.Text= _temps[0];
+            if (_temps[1] == "<")
+                comboBox1.SelectedIndex = 0;
+            else if (_temps[1] == ">")
+                comboBox1.SelectedIndex = 1;
+            else if (_temps[1] == "=")
+                comboBox1.SelectedIndex = 2;
+            else if (_temps[1] == "<=")
+                comboBox1.SelectedIndex = 3;
+            else if (_temps[1] == ">=")
+                comboBox1.SelectedIndex = 4;
+            else if (_temps[1] == "<>")
+                comboBox1.SelectedIndex = 5;
+            for(int i = 0; i < 5; i++)
+            {
+                if (Convert.ToDouble(_temps[2]) == Convert.ToDouble(listBoxSecOp.Items[i]))
+                {
+                    listBoxSecOp.SelectedIndex = i;
+                    break;
+                }
+            }
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            comboBox1.SelectedIndex = (-1);
+            listBoxSecOp.SelectedIndex = (-1);
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
